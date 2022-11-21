3bc064ffb7fb6ef4b57d78d4a2bd3d2e7f26e14e
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-11-21 12:08:50 +0300

    reducer

---
+++ reducer-example/reducer-starter/src/reducer.js
@@ -0,0 +1,16 @@
+export const initialState = {
+  loading: false,
+  catImage: "",
+  error: "",
+};
+
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case "START":
+      break;
+    case "SUCCESS":
+    case "FAIL":
+    default:
+      break;
+  }
+};
